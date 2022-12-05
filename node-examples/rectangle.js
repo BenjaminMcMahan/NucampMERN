5af7194e053c7ed71fe6f452cca2c0b8c8648155
Benjamin McMahan <108589431+BenjaminMcMahan@users.noreply.github.com>
2022-12-05 15:10:58 -0500

    Exercise: Callbacks and Error Handling in Node

--- node-examples/rectangle.js
+++ node-examples/rectangle.js
@@ -1,2 +1,16 @@
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => x * y;
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        // Error...
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
+    } else {
+        // Simulate synchronous function
+        setTimeout(() => {
+            // No error
+            callback(null, {
+                // Closure. This inner function has access to the parameters of the outer function
+                perimeter: () => 2 * (x + y),
+                area: () => x * y
+            });
+        }, 2000);
+    }
+};
