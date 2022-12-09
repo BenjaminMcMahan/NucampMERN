33300d8f6ad89364da03fa95f149bb5c7e08c8cf
Benjamin McMahan <108589431+BenjaminMcMahan@users.noreply.github.com>
2022-12-09 16:15:29 -0500

    Exercise - Intro to Express Part II

--- node-express/server.js
+++ node-express/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev')); // Log using the dev version
+
+// Server static files from the public directory
+app.use(express.static(__dirname + '/public'));
 
 app.use((
     // Middleware
     req, res
 ) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html lang="en"><body><h1>This is an Express Server</h1></body></html>');
