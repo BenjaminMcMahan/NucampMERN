7aac47b0b08271d5d24de0199b5596c4bc5e203a
Benjamin McMahan <108589431+BenjaminMcMahan@users.noreply.github.com>
2022-12-26 14:04:50 -0500

    Exercise - User Auth with Passport and JSON Web Token

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -2,10 +2,8 @@ let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let logger = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session); // TODO review first class functions
 const passport = require('passport');
-const authenticate = require('./authenticate');
+const config = require('./config');
 const mongoose = require('mongoose');
 
 let indexRouter = require('./routes/index');
@@ -14,7 +12,8 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
-const url = "mongodb://127.0.0.1:27017/nucampsite"; // Recent updates, localhost does not work
+const url = config.mongoUrl; // Recent updates, localhost does not work
+
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
@@ -35,38 +34,16 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(session({
-    name: 'session-id',
-    secret: 'ophelia-82-89-339922-1583',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
-}));
 
 // Auth
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-    console.log(req.user);
-
-    if (!req.user) {
-        console.log(req.session); // Log the current session details
-        // False if the cookies is not assigned
-        const err = new Error('You are not authenticated!');
-        err.status = 401;
-        return next(err);
-    } else {
-       return next(); // Pass to next middleware
-    }
-}
-
-app.use(auth); // Note that middleware is cascading
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Routers
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
--- nucampsiteServer/authenticate.js
+++ nucampsiteServer/authenticate.js
@@ -1,8 +1,45 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken');
+
+const config = require('./config.js');
 
 // Using session based authentication
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = user => {
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600}); // Expires in one hour
+};
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); // Server expects a token to be set
+opts.secretOrKey = config.secretKey;
+
+// Export the JWT strategy
+exports.jwtPassport = passport.use(
+    new JwtStrategy(
+        // Options
+        opts,
+        // Verify callback function
+        (jwt_payload, done) => {
+            console.log('JWT Payload:', jwt_payload);
+            User.findOne({_id: jwt_payload._id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                } else if (user) {
+                    return done(null, user);
+                } else {
+                    return done(null); // No error, but no user was found
+                    // This could be a place to ask about creating a new user
+                }
+            })
+        }
+    )
+);
+
+exports.verifyUser = passport.authenticate('jwt', {session:false});
