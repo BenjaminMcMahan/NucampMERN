ffba5cfb007287d937decb94c6f49f6b872fecbf
Benjamin McMahan <108589431+BenjaminMcMahan@users.noreply.github.com>
2022-12-27 12:23:40 -0500

    Exercise - Uploading Files

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -11,6 +11,7 @@ let usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const url = config.mongoUrl; // Recent updates, localhost does not work
 
@@ -57,6 +58,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
